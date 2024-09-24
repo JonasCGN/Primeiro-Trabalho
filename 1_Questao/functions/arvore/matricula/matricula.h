b1986efd4f2ab6e4544246fd94c3bed3469f80eb
JonasCGN <jonasbo66@gmail.com>
2024-09-23 23:59:03 -0300

    Corrigindo arquios

--- 1_Questao/functions/arvore/matricula/matricula.h
+++ 1_Questao/functions/arvore/matricula/matricula.h
@@ -4,6 +4,4 @@
 #include "../../../struct/lista_aluno.h"
 
 int inserirMatricula(ArvoreMatricula** raiz, int codDisciplina);
-void removerDisciplinaMatricula(ArvoreMatricula** raiz, int codDisciplina);
-ArvoreMatricula *filhoArvore(ArvoreMatricula *arvoreMatricula);
-int folhaArvore(ArvoreMatricula *arvoreMatricula);
+int removerDisciplinaMatricula(ArvoreMatricula **raiz, int codDisciplina);
